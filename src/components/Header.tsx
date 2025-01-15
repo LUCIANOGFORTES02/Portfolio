b07397d44d55c386c39556a9d5a258eb6dcf35d4
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-01-15 12:45:57 -0300

    fix: style components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center py-4">
-      <div className='text-lg text-secondary'>Luciano Guimarães Fortes</div>
-      <div className="flex gap-4">
+    <header className="flex justify-between items-center py-4 ">
+      <div className='text-lg text-secondary'>Luciano Fortes</div>
+      <div className="flex gap-2">
         <a href="#about" className="hover:text-purple-600"> 
           <span>Sobre mim</span>
         </a>
